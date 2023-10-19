94398e1d186f12fd5e36abfd146285f8c4eb6ecf
gitaepark97 <parkkitae7@gmail.com>
2023-10-19 17:30:51 +0900

    feat: database transaction
    DB transaction lock & how to handle deadlock in Golang

--- db/query/account.sql
+++ db/query/account.sql
@@ -15,6 +15,14 @@ FROM accounts
 WHERE id = $1
 LIMIT 1;
 
+-- name: GetAccountForUpdate :one
+SELECT
+  *
+FROM accounts
+WHERE id = $1
+LIMIT 1
+FOR NO KEY UPDATE;
+
 -- name: ListAccounts :many
 SELECT
   *
@@ -27,6 +35,13 @@ UPDATE accounts
 SET
   balance = $2
 WHERE id = $1
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET
+  balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
 RETURNING *;
 
 -- name: DeleteAccount :exec
